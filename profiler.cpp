ae315483ad55a3f8af0c148aa850de99ebdda6a8
Tilen Stermecki <tilen.stermecki@gmail.com>
2025-03-06 17:01:51 +0100

    partly working, names not working yet

--- profiler.cpp
+++ profiler.cpp
@@ -1,126 +1,148 @@
 #include "profiler.h"
-#include <iostream>
-#include <thread>
-#include <sstream>
-#include <cstdlib>
 
-#ifdef _WIN32
-    #include <windows.h>
-    #define GET_PROGRAM_NAME() ([](){ char path[MAX_PATH]; GetModuleFileNameA(NULL, path, MAX_PATH); return std::string(path); }())
-#else
-    #include <unistd.h>
-    #include <libgen.h> // For basename()
-    extern char* program_invocation_name;
-    #define GET_PROGRAM_NAME() (std::string(program_invocation_name))
-#endif
-
-std::string program_name = GET_PROGRAM_NAME();
-
-std::unordered_map<std::string, size_t> profiler_call_count;
-std::unordered_map<std::string, double> profiler_total_time;
-std::mutex profiler_mutex;
-std::thread::id profiler_main_thread_id;
-
-void profiler_init() {
-    profiler_main_thread_id = std::this_thread::get_id();
-    std::cout << "[Profiler] Auto setup complete. Running profiling on " << program_name << "...\n";
-}
-
-bool profiler_is_main_thread() {
-    return std::this_thread::get_id() == profiler_main_thread_id;
-}
-
-std::string profiler_resolve_function_name(void* func) {
-    static std::unordered_map<void*, std::string> cache;
-    static std::mutex cache_mutex;
-
-    std::lock_guard<std::mutex> lock(cache_mutex);
-
-    if (cache.find(func) != cache.end()) {
-        return cache[func];
-    }
-
-    std::ostringstream command;
-    command << "addr2line -e " << program_name << " -f -p " << func;
-
-    FILE* pipe = popen(command.str().c_str(), "r");
-    if (!pipe) return "Unknown Function";
-
-    char buffer[256];
-    std::string result;
-    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-        result += buffer;
-    }
-    pclose(pipe);
-
-    result.erase(result.find_last_not_of("\n") + 1);
-    cache[func] = result;
-    return result;
-}
-
-void profiler_record(const std::string& functionName, double duration) {
-    std::lock_guard<std::mutex> lock(profiler_mutex);
-    profiler_call_count[functionName]++;
-    profiler_total_time[functionName] += duration;
-}
-
-void profiler_report() {
-    std::cout << "\n--- Profiling Report (Depth 1) ---\n";
-    for (const auto& [func, calls] : profiler_call_count) {
-        double totalTime = profiler_total_time[func];
-        std::cout << func << " | Calls: " << calls
-                  << " | Total Time: " << totalTime << "s"
-                  << " | Avg Time: " << (totalTime / calls) << "s\n";
-    }
-}
+extern string program_name;
 
 // Function enter hook
-extern "C" void __cyg_profile_func_enter(void* func, void* callsite) { // TODO infinite call loop
-    (void)callsite;
+extern "C" void __cyg_profile_func_enter(void *func, void *callsite) {
+	(void)callsite;
 
-    static thread_local int depth = 0;
-    static bool first_call = true;
+	if (disable_profiling)
+		return;
 
-    if (first_call) {
-        const char* msg = "First function entered!\n";
-        write(STDOUT_FILENO, msg, 23);  // Directly print without using C++ runtime
-        first_call = false;
-    } else if (profiler_is_main_thread() && depth == 0) {
-        std::string functionName = profiler_resolve_function_name(func);
-        static thread_local std::unordered_map<std::string, std::chrono::high_resolution_clock::time_point> startTimes;
-        startTimes[functionName] = std::chrono::high_resolution_clock::now();
-    }
+	disable_profiling = true;
 
-    depth++;
+	if (depth < 0) {
+		if (func == main) {
+			profiler_init();
+		}
+		depth += func == main;
+
+		disable_profiling = false;
+		return;
+	}
+
+	depth++;
+
+	if (depth > PROFILER_SET_MAX_DEPTH) {
+		disable_profiling = false;
+		return;
+	}
+
+	string functionName = profiler_resolve_function_name(func);
+	cout << functionName << endl;
+	startTimes[functionName] = chrono::high_resolution_clock::now();
+
+	disable_profiling = false;
 }
 
 // Function exit hook
-extern "C" void __cyg_profile_func_exit(void* func, void* callsite) {
-    (void)callsite;
+extern "C" void __cyg_profile_func_exit(void *func, void *callsite) {
+	(void)callsite;
 
-    static thread_local int depth = 0;
-    if (depth <= 0) return;
-    depth--;
+	if (disable_profiling)
+		return;
 
-    if (profiler_is_main_thread() && depth == 0) {
-        auto endTime = std::chrono::high_resolution_clock::now();
-        std::string functionName = profiler_resolve_function_name(func);
+	if (depth < 0) {
+		return;
+	}
 
-        static thread_local std::unordered_map<std::string, std::chrono::high_resolution_clock::time_point> startTimes;
-        if (startTimes.find(functionName) != startTimes.end()) {
-            std::chrono::duration<double> elapsed = endTime - startTimes[functionName];
-            profiler_record(functionName, elapsed.count());
-            startTimes.erase(functionName);
-        }
-    }
+	disable_profiling = true;
+
+	if (depth == 0) {  // Exited main
+		profiler_report();
+		depth--;
+		disable_profiling = false;
+		return;
+	}
+
+	auto endTime = chrono::high_resolution_clock::now();
+	string functionName = profiler_resolve_function_name(func);
+
+	if (startTimes.find(functionName) != startTimes.end()) {
+		chrono::duration<double> elapsed = endTime - startTimes[functionName];
+		profiler_record(functionName, elapsed.count());
+		startTimes.erase(functionName);
+	}
+
+	depth--;
+
+	disable_profiling = false;
 }
 
-// 🔥 Report results automatically at program end
-struct ProfilerFinalizer {
-    ~ProfilerFinalizer() { profiler_report(); }
-} __profiler_finalizer;
+void profiler_init() {
+	cout << "[Profiler] Auto setup complete, hooks are working.\n";
+	get_program_base_address();
+}
 
-// Initialize profiler automatically
-struct ProfilerInitializer {
-    ProfilerInitializer() { profiler_init(); }
-} __profiler_initializer;
+string profiler_resolve_function_name(void *func) {
+	lock_guard<mutex> lock(cache_mutex);
+
+	if (cache.find(func) != cache.end()) {
+		return cache[func];
+	}
+
+	ostringstream command;
+	command << "addr2line -e " << program_name << " -f -p " << func;
+	cout << "addr2line -e " << program_name << " -f -p " << func << endl;
+
+	FILE *pipe = popen(command.str().c_str(), "r");
+	if (!pipe)
+		return "Unknown Function";
+
+	char buffer[256];
+	string result;
+	while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+		result += buffer;
+	}
+	pclose(pipe);
+
+	result.erase(result.find_last_not_of("\n") + 1);
+	cache[func] = result;
+	return result;
+}
+
+void profiler_record(const string &functionName, double duration) {
+	lock_guard<mutex> lock(profiler_mutex);
+	profiler_call_count[functionName]++;
+	profiler_total_time[functionName] += duration;
+}
+
+void profiler_report() {
+	cout << "\n--- Profiling Report (Max Depth = " << PROFILER_SET_MAX_DEPTH << ") ---\n";
+	for (const auto &[func, calls] : profiler_call_count) {
+		double totalTime = profiler_total_time[func];
+		cout << func << " | Calls: " << calls
+			 << " | Total Time: " << totalTime << "s"
+			 << " | Avg Time: " << (totalTime / calls) << "s\n";
+	}
+}
+
+
+
+
+#ifdef _WIN32
+#error "This code is not yet compatible with Windows!"
+#elif __linux__
+
+string get_program_base_address() {
+	ostringstream command;
+	command << "cat /proc/self/maps";
+	FILE *pipe = popen(command.str().c_str(), "r");
+	if (!pipe)
+		return "Unknown Function";
+
+	char buffer[256];
+	string result;
+	while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+		result += buffer;
+	}
+	pclose(pipe);
+
+	cout << result << endl;
+	return string(result);
+}
+
+#define GET_PROGRAM_BASE_ADDRESS
+#else
+#error "This code is not compatible with your OS!"
+#endif
--- profiler.h
+++ profiler.h
@@ -1,20 +1,64 @@
 #pragma once
 
-#include <chrono>
-#include <unordered_map>
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sstream>
 #include <string>
+#include <libgen.h>	 // For basename()
+#include <unordered_map>
 #include <mutex>
+#include <thread>
+#include <chrono>
+#include <dlfcn.h> // for f. addr. to name
+#include <cxxabi.h> // to demangle names // TODOOOOOOOOOOO
 
-inline void profiler_init() __attribute__((no_instrument_function));
-inline void profiler_record(const std::string& functionName, double duration) __attribute__((no_instrument_function));
-inline void profiler_report() __attribute__((no_instrument_function));
-inline bool profiler_is_main_thread() __attribute__((no_instrument_function));
-inline std::string profiler_resolve_function_name(void* func) __attribute__((no_instrument_function));
+using namespace std;
 
-extern "C" void __cyg_profile_func_enter(void* func, void* callsite) __attribute__((no_instrument_function));;
-extern "C" void __cyg_profile_func_exit(void* func, void* callsite) __attribute__((no_instrument_function));;
+#ifdef _WIN32
+#include <windows.h>
+#define GET_PROGRAM_NAME() ([]() {             \
+    char path[MAX_PATH];                       \
+    GetModuleFileNameA (NULL, path, MAX_PATH); \
+    return string (path); }())
+#elif __linux__
+extern char *program_invocation_name;
+#define GET_PROGRAM_NAME() (string(program_invocation_name))
+#else
+#error "This code is not compatible with your OS!"
+#endif
 
-extern std::string program_name;
+#ifndef PROFILER_SET_MAX_DEPTH
+#define PROFILER_SET_MAX_DEPTH 1
+#endif
 
-// Include implementation at the end
+bool disable_profiling = false;
+thread_local int depth = -1;  // -1 means not yet in main
+
+string program_name = GET_PROGRAM_NAME();
+
+unordered_map<string, size_t> profiler_call_count;
+unordered_map<string, double> profiler_total_time;
+mutex profiler_mutex;
+
+unordered_map<void *, string> cache;
+mutex cache_mutex;
+
+
+thread_local unordered_map<string, chrono::high_resolution_clock::time_point> startTimes;
+
+int main(int argc, char *argv[]);
+
+__attribute__((no_instrument_function)) void profiler_init();
+
+__attribute__((no_instrument_function)) void profiler_record(const string &functionName, double duration);
+__attribute__((no_instrument_function)) void profiler_report();
+
+__attribute__((no_instrument_function)) string profiler_resolve_function_name(void *func);
+__attribute__((no_instrument_function)) string get_program_base_address();
+
+
+extern "C" __attribute__((no_instrument_function)) void __cyg_profile_func_enter(void *func, void *callsite);
+extern "C" __attribute__((no_instrument_function)) void __cyg_profile_func_exit(void *func, void *callsite);
+
 #include "profiler.cpp"
