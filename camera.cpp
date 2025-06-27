dbc2c19251281dcab67cd1cfad84e3f18f9b78c6
TilenS <tilen.stermecki@gmail.com>
2025-06-27 23:35:44 +0200

    small fixes

--- camera.cpp
+++ camera.cpp
@@ -1,4 +1,5 @@
 #include "camera.h"
+
 void Camera::assignRenderer(SDL_Renderer *_r) {
     r = _r;
     SDL_GetRendererOutputSize(r, &w, &h);
--- camera.h
+++ camera.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 
 using namespace std;
 
